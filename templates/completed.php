2d6c3ae54df48a5c128c27ad9aa21c9ec8135ef7
Agustin Kassis <webmaster@masize.com>
2024-01-10 19:34:28 -0300

    removed unnecessary components on completed

--- templates/completed.php
+++ templates/completed.php
@@ -1,14 +1,5 @@
 <h2><?=__('Payment completed successfully', 'lawallet-woocommerce')?></h2>
 <ul class="order_details">
-  <?/*
-  <li>
-    <?=__('Payment completed at', 'lawallet-woocommerce')?>: <strong><?=date('r', $invoiceRep->settle_date) ?></strong>
-  </li>
-  */
-  ?>
-  <li>
-    <?=__('Lightning hash', 'lawallet-woocommerce')?>: <strong><?=$payHash ?></strong>
-  </li>
   <li>
     <?=__('Invoice amount', 'lawallet-woocommerce')?>: <strong><?=self::format_msat($sats) ?></strong>
   </li>
