6e8426d5c025a8e284f08b133b46080b0932afb9
Agustin Kassis <webmaster@masize.com>
2024-01-10 19:34:04 -0300

    fixed payment template

--- templates/payment.php
+++ templates/payment.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" href="<? echo plugins_url('assets/css/payment.css', dirname(__FILE__))?>" type="text/css">
+
+<link rel="stylesheet" href="<?=plugins_url('assets/css/payment.css', dirname(__FILE__))?>" type="text/css" />
 <noscript><style>.yesscript{display:none}</style></noscript>
 
 <div class="ln-pay">
@@ -12,9 +13,13 @@
   </h4>
   <img class="qr" src="<?php echo $qr_uri ?>">
   <code class="payreq"><?php echo $payReq ?></code>
+  <hr />
+  <p>
+    <code><?php echo json_encode($lud16) ?></code>
+  </p>
   <p>
     <noscript>Your browser has JavaScript turned off. Please refresh the page manually after making the payment.</noscript>
-    <span class="yesscript"><img src="<? echo plugins_url( '../assets/img/loader.gif', __FILE__ ) ?>" class="loader" alt="loading">
+    <span class="yesscript"><img src="<?=plugins_url( '../assets/img/loader.gif', __FILE__ ) ?>" class="loader" alt="loading">
       <span id="invoice_expires_label">
         <?=__('Awaiting payment', 'lawallet-woocommerce')?>.
         <?=__('The invoice expires', 'lawallet-woocommerce')?> <span id="expiry-timer" title="<?=$expiry ?>"><?=$expiry ?></span>.
@@ -24,56 +29,62 @@
   <a class="checkout-button button alt btn btn-default" href="lightning:<?=$payReq; ?>"><?=__('Pay with Lightning', 'lawallet-woocommerce')?></a>
 </div>
 
-<script>
-(function($, ajax_url, invoice_id, redir_url, expires_at){
-  var first_check = false;
-  $(function poll() {
-    $.post(ajax_url, { action: 'ln_wait_invoice', invoice_id: invoice_id })
-      .success((code, state, res) => {
-        first_check = true;
-        if (res.responseJSON === true) {
-          playPayedAnimation();
-          return document.location = redir_url;
-        }
+<script type="text/javascript">
+  (function($, ajax_url, invoice_id, redir_url, expires_at){
+    var first_check = false;
 
-        setTimeout(poll, 10000);
-        throw new Error('succesful response, but not the expected one')
-      })
-      .fail(res => {
-        // 402 Payment Required: timeout reached without payment, invoice is still payable
-        if (res.status === 402) return poll()
-        // 410 Gone: invoice expired and can not longer be paid
-        if (res.status === 410) return location.reload()
+    var intervalId = null;
 
-        throw new Error('unexpected status code '+res.status)
-      })
-  })
+    function poll() {
+      $.post(ajax_url, { action: 'ln_wait_invoice', invoice_id: invoice_id })
+        .success((code, state, res) => {
+          first_check = true;
+          if (res.responseJSON === true) {
+            playPayedAnimation();
+            intervalId && clearInterval(intervalId);
+            return document.location = redir_url;
+          }
+          throw new Error('succesful response, but not the expected one')
+        })
+        .fail(res => {
+          // 402 Payment Required: timeout reached without payment, invoice is still payable
+          // if (res.status === 402) return poll()
+          // 410 Gone: invoice expired and can not longer be paid
+          if (res.status === 410) return location.reload()
 
-  ;(function updateExpiry() {
-    var left = expires_at - (+new Date()/1000|0)
-    if (left <= 0) {
-      if (first_check) return location.reload();
-      $('#invoice_expires_label').text('<?=__('Generating new invoice', 'lawallet-woocommerce')?>...');
-    } else {
-      $('#expiry-timer').text('<?=__('in', 'lawallet-woocommerce')?> '+formatDur(left))
+          throw new Error('unexpected status code '+res.status)
+        })
+    };
+    
+    function updateExpiry() {
+      var left = expires_at - (+new Date()/1000|0)
+      if (left <= 0) {
+        alert("RESTARTING");
+        if (first_check) return location.reload();
+        $('#invoice_expires_label').text('<?=__('Generating new invoice', 'lawallet-woocommerce')?>...');
+      } else {
+        $('#expiry-timer').text('<?=__('in', 'lawallet-woocommerce')?> '+left)
+      }
+      setTimeout(updateExpiry, 1000)
     }
-    setTimeout(updateExpiry, 1000)
-  })()
 
-  function playPayedAnimation() {
-    $('.ln-pay .check').css({
-      display: 'block'
-    });
-    $('.ln-pay .qr').css({
-      opacity: 0.3
-    });
-  }
+    function playPayedAnimation() {
+      $('.ln-pay .check').css({
+        display: 'block'
+      });
+      $('.ln-pay .qr').css({
+        opacity: 0.3
+      });
+    }
 
-  function formatDur(x) {
-    var h=x/3600|0, m=x%3600/60|0, s=x%60
-    return ''+(h>0?h+':':'')+(m<10&&h>0?'0':'')+m+':'+(s<10?'0':'')+s
-  }
-})(jQuery, <? echo json_encode(admin_url( 'admin-ajax.php' )) ?>, <? echo json_encode($order->get_id()) ?>,
-           <? echo json_encode($order->get_checkout_order_received_url()) ?>, <?=$expiry ?>)
+    $(function () {
+      intervalId = setInterval(() => {
+        poll();
+      }, 2000);
+    })
+    
+
+  })(jQuery, <?=json_encode(admin_url( 'admin-ajax.php' )) ?>, <?=json_encode($order->get_id()) ?>,
+            <?=json_encode($order->get_checkout_order_received_url()) ?>, <?=$expiry ?>)
 
 </script>
